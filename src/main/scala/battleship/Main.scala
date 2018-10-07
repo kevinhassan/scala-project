70066eb65fc32a20dd6faaf5891d8f138b8d0f0c
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2018-10-07 13:07:40 +0200

    edit: refactor main
    Signed-off-by: Kévin Hassan <kevin.hassan@etu.umontpellier.fr>

--- src/main/scala/battleship/Main.scala
+++ src/main/scala/battleship/Main.scala
@@ -13,10 +13,10 @@ object Main extends App {
   def chooseMode(): Unit = {
     val optionChosen: Int = Utils.askOptions
     optionChosen match {
-      case 1 => playerVsPlayer()
-      case 2 => playerVsIa("easy")
-      case 3 => playerVsIa("medium")
-      case 4 => playerVsIa("hard")
+      case 1 => humanMode()
+      case 2 => humanMode("easy")
+      case 3 => humanMode("medium")
+      case 4 => humanMode("hard")
       case 5 => iaVsIa("easy", "medium")
       case 6 => iaVsIa("medium", "hard")
       case 7 => iaVsIa("hard", "easy")
@@ -33,67 +33,18 @@ object Main extends App {
     *
     * @return
     */
-  def playerVsPlayer(): GameState = {
+  def humanMode(difficulty: String = ""): GameState = {
     val username1: String = Utils.askUsername(1)
-    val username2: String = Utils.askUsername(2)
+    // no difficulty implies player vs player mode
+    val username2: String = if (difficulty.isEmpty) Utils.askUsername(2) else difficulty
     // create player with his fleet of ship
-    val player1 = Utils.createFleet(Player(username1, Grid(gridSize), isHuman = true))
+    val player1: Player = Utils.createFleet(Player(username1, Grid(gridSize), isHuman = true))
     Utils.clearConsole()
-    val player2 = Utils.createFleet(Player(username2, Grid(gridSize), isHuman = true))
+    val player2: Player = if (difficulty.isEmpty) Utils.createFleet(Player(username2, Grid(gridSize), isHuman = true)) else Utils.createFleet(Player(username2, Grid(gridSize), isHuman = false))
     Utils.clearConsole()
     val gameState: GameState = GameState(Set(player1, player2), player1)
 
-    def playerVsPlayerTailRec(g: GameState): GameState = {
-      // take the active player of the game
-      val activePlayer: Player = g.getActivePlayer
-      val opponentPlayer: Player = g.getOpponentPlayer
-      // display grids
-      Utils.displayGridBoats(activePlayer)
-      Utils.displayGridShots(activePlayer)
-      // indicate the coords where the grid will be shoot
-      val coords: (Int, Int) = Utils.askShotPosition(activePlayer)
-      // result of the shot done
-      val isShot: Boolean = opponentPlayer.grid.isTouched(coords)
-      // print the result of the shot
-      if (isShot) Utils.printMessage("Hit !") else Utils.printMessage("Miss !")
-      // add the shot done by the active player to the opponent
-      val nOpponentPlayer: Player = opponentPlayer.takeAShot(coords)
-      // add the position and its result to the active player
-      val nActivePlayer: Player = activePlayer.makeAShot((coords._1, coords._2, isShot))
-      // check if the opponentPlayer has at least one ship on his grid
-      val nGameState: GameState = g.copy(players = Set(nActivePlayer, nOpponentPlayer))
-      // check if the opponent has always a ship
-      if (nGameState.isFinish) {
-        // finish the game and print the gameState score
-        Utils.printMessage(s"Player ${nActivePlayer.username} win !")
-        Utils.printMessage(s"Player ${nActivePlayer.username} (${nActivePlayer.score + 1}) - Player ${nOpponentPlayer.username} (${nOpponentPlayer.score})")
-        if (Utils.askToRestart) {
-          val p1: Player = nActivePlayer.copy(score = nActivePlayer.score + 1)
-          val np1: Player = Utils.createFleet(p1.resetPlayer)
-          Utils.clearConsole()
-          val p2: Player = Utils.createFleet(nOpponentPlayer.resetPlayer)
-          playerVsPlayerTailRec(nGameState.copy(players = Set(p2, np1), launcher = p2))
-        } else {
-          g
-        }
-      } else {
-        // we change the round
-        playerVsPlayerTailRec(nGameState.nextRound)
-      }
-    }
-
-    playerVsPlayerTailRec(gameState)
-  }
-
-  def playerVsIa(difficulty: String): GameState = {
-    val username1: String = Utils.askUsername(1)
-    // create player with his fleet of ship
-    val player1 = Utils.createFleet(Player(username1, Grid(gridSize), isHuman = true))
-    Utils.clearConsole()
-    val player2 = Utils.createFleet(Player(difficulty, Grid(gridSize), isHuman = false))
-    val gameState: GameState = GameState(Set(player1, player2), player1)
-
-    def playerVsIaTailRec(g: GameState): GameState = {
+    def humanModeTailRec(g: GameState): GameState = {
       // take the active player of the game
       val activePlayer: Player = g.getActivePlayer
       val opponentPlayer: Player = g.getOpponentPlayer
@@ -118,25 +69,30 @@ object Main extends App {
       if (nGameState.isFinish) {
         // finish the game and print the gameState score
         if (nActivePlayer.isHuman) {
-          Utils.printMessage(s"Player ${nActivePlayer.username} (${nActivePlayer.score + 1}) - Player IA-${nOpponentPlayer.username} (${nOpponentPlayer.score})")
+          if (!nOpponentPlayer.isHuman) {
+            Utils.printMessage(s"Player ${nActivePlayer.username} (${nActivePlayer.score + 1}) - Player IA-${nOpponentPlayer.username} (${nOpponentPlayer.score})")
+          } else {
+            Utils.printMessage(s"Player ${nActivePlayer.username} (${nActivePlayer.score + 1}) - Player ${nOpponentPlayer.username} (${nOpponentPlayer.score})")
+          }
         } else {
-          Utils.printMessage(s"Player IA-${nActivePlayer.username} (${nActivePlayer.score + 1}) - Player ${nOpponentPlayer.username} (${nOpponentPlayer.score})")
+          Utils.printMessage(s"Player IA-${nActivePlayer.username} (${nActivePlayer.score + 1}) - ${nOpponentPlayer.username} (${nOpponentPlayer.score})")
         }
         if (Utils.askToRestart) {
           val p1: Player = nActivePlayer.copy(score = nActivePlayer.score + 1)
           val np1: Player = Utils.createFleet(p1.resetPlayer)
           Utils.clearConsole()
           val p2: Player = Utils.createFleet(nOpponentPlayer.resetPlayer)
-          playerVsIaTailRec(nGameState.copy(players = Set(p2, np1), launcher = p2))
+          humanModeTailRec(nGameState.copy(players = Set(p2, np1), launcher = p2))
         } else {
           g
         }
       } else {
         // we change the round
-        playerVsIaTailRec(nGameState.nextRound)
+        humanModeTailRec(nGameState.nextRound)
       }
     }
-    playerVsIaTailRec(gameState)
+
+    humanModeTailRec(gameState)
   }
 
   def iaVsIa(difficultyJ1: String, difficultyJ2: String): GameState = {
